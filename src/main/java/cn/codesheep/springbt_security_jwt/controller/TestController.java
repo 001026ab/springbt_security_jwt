b89f3bfd387006972fee23d032fc84103706aa5b
001026ab <1176754512@qq.com>
2022-02-28 10:55:06 +0800

    测试权限控制

--- src/main/java/cn/codesheep/springbt_security_jwt/controller/TestController.java
+++ src/main/java/cn/codesheep/springbt_security_jwt/controller/TestController.java
@@ -6,23 +6,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @www.codesheep.cn
- * 20190312
+ * @www.codesheep.cn 20190312
  */
 @RestController
 public class TestController {
 
+    /**
+     * 注意hasAnyAuthority与hasAuthority的区别，前者可以同时配置多个权限角色，后者只能配置一个
+     *
+     * @return
+     */
+
     // 测试普通权限
-    @PreAuthorize("hasAuthority('ROLE_NORMAL')")
-    @RequestMapping( value="/normal/test", method = RequestMethod.GET )
+    @PreAuthorize("hasAnyAuthority('ROLE_NORMAL','ROLE_ADMIN')")
+    @RequestMapping(value = "/normal/test", method = RequestMethod.GET)
     public String test1() {
         return "ROLE_NORMAL /normal/test接口调用成功！";
     }
 
     // 测试管理员权限
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    @RequestMapping( value = "/admin/test", method = RequestMethod.GET )
+    @RequestMapping(value = "/admin/test", method = RequestMethod.GET)
     public String test2() {
+        return "ROLE_ADMIN /admin/test接口调用成功！";
+    }
+
+    // 测试其他
+    @PreAuthorize("hasAuthority('ROLE_ELSE')")
+    @RequestMapping(value = "/else/test", method = RequestMethod.GET)
+    public String test3() {
         return "ROLE_ADMIN /admin/test接口调用成功！";
     }
 }
