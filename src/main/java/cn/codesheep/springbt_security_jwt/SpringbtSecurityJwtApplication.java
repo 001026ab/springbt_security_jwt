c0a97551389efef35c4be70d881dd3a8d8b65226
001026ab <1176754512@qq.com>
2022-02-28 10:38:30 +0800

    初始提交

---
+++ src/main/java/cn/codesheep/springbt_security_jwt/SpringbtSecurityJwtApplication.java
@@ -0,0 +1,17 @@
+package cn.codesheep.springbt_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ufi
+ * @www.codesheep.cn
+ * 20190312
+ */
+@SpringBootApplication
+public class SpringbtSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbtSecurityJwtApplication.class, args);
+    }
+}
